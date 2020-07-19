216ac954b20e8dd64adf8583831598d9009bd016
nana <332161019@qq.com>
2020-07-19 16:37:23 +0800

    springboot+jpa模板

---
+++ luckymoney/src/main/java/com/tools/luckymoney/LuckmoneyRepository.java
@@ -0,0 +1,10 @@
+package com.tools.luckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by mana
+ * 2019-03-11 21:38
+ */
+public interface LuckmoneyRepository extends JpaRepository<Luckymoney, Integer> {
+}
